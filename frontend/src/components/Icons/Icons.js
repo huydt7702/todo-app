0f10827b7d30d23eb627322df1d775d5e9a45940
huydt7702 <developer.vanhuy@gmail.com>
2023-04-13 15:14:39 +0700

    build header ui

--- frontend/src/components/Icons/Icons.js
+++ frontend/src/components/Icons/Icons.js
@@ -14,3 +14,33 @@ export const ArrowDownIcon = ({ width = '2.4rem', height = '2.4rem', className }
         ></path>
     </svg>
 );
+
+export const SearchIcon = ({ width = '2.4rem', height = '2.4rem', className }) => (
+    <svg
+        width={width}
+        height={height}
+        className={className}
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <g>
+            <path d="M20.87,20.17l-5.59-5.59C16.35,13.35,17,11.75,17,10c0-3.87-3.13-7-7-7s-7,3.13-7,7s3.13,7,7,7c1.75,0,3.35-0.65,4.58-1.71 l5.59,5.59L20.87,20.17z M10,16c-3.31,0-6-2.69-6-6s2.69-6,6-6s6,2.69,6,6S13.31,16,10,16z"></path>
+        </g>
+    </svg>
+);
+
+export const ClearIcon = ({ width = '2.4rem', height = '2.4rem', className }) => (
+    <svg
+        width={width}
+        height={height}
+        className={className}
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <g>
+            <path d="M12.7,12l6.6,6.6l-0.7,0.7L12,12.7l-6.6,6.6l-0.7-0.7l6.6-6.6L4.6,5.4l0.7-0.7l6.6,6.6l6.6-6.6l0.7,0.7L12.7,12z"></path>
+        </g>
+    </svg>
+);
--- frontend/src/layouts/components/Header/Header.js
+++ frontend/src/layouts/components/Header/Header.js
@@ -1,7 +1,36 @@
+import { Link } from 'react-router-dom';
+import { ClearIcon, SearchIcon } from '~/components/Icons';
+import Image from '~/components/Image/Image';
+import config from '~/config';
+
 function Header() {
     return (
-        <header className="wrapper">
-            <h1>Header</h1>
+        <header className="flex items-center h-[48px] bg-[#2564cf] justify-between px-4">
+            <Link to={config.routes.home} className="font-semibold text-white hover:underline">
+                To Do
+            </Link>
+
+            <div className="relative flex items-center w-[400px]">
+                <span className="absolute top-0 lef-0 bottom-0 flex items-center justify-center px-[8px] cursor-pointer text-[#2564cf] hover:bg-[rgba(0,0,0,0.05)] transition-colors">
+                    <SearchIcon />
+                </span>
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="flex-1 py-[8px] px-[40px] h-[32px] rounded-md text-xl"
+                />
+                <span className="absolute top-0 bottom-0 right-0 flex items-center justify-center px-[8px] cursor-pointer text-[#2564cf] hover:bg-[rgba(0,0,0,0.05)] transition-colors">
+                    <ClearIcon />
+                </span>
+            </div>
+
+            <div className="flex items-center justify-center h-full w-[48px] hover:bg-[#005A9E] transition-colors cursor-pointer">
+                <Image
+                    className="rounded-full w-[32px] h-[32px] object-cover border-[1px] border-solid border-white"
+                    src="https://yt3.ggpht.com/yti/AHyvSCB28-vRqPO18qUYbFvjBwj7JvCFh9Bh4dlOQWe1DA=s88-c-k-c0x00ffffff-no-rj-mo"
+                    alt="User"
+                />
+            </div>
         </header>
     );
 }
